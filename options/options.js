d06c06e88647e4702c90363aa38f3ccc2ef30885
Crowdbotics <team@crowdbotics.com>
2023-12-07 13:42:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "marking-app-44885",
+  url: "https://marking-app-44885.botics.co",
+  api: "https://marking-app-44885.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
